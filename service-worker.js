feee9d1d0035092b7250f4b07290a0743ebe5fda
Eduard-R <edi_r@gmx.de>
2025-05-19 23:15:04 +0200

    Convert to PWA

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index_cards.html',
+  '/styles_cards.css',
+  '/app.js',
+  '/icons/icon.png',
+  '/img/Background.png',
+  '/img/Boldness.png',
+  '/img/Flight.png',
+    '/img/Healing.png',
+    '/img/Resilience.png',
+    '/img/Swiftness.png',
+    '/img/vintage-grunge.jpg'
+];
+
+// Installieren
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+// Abrufen
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
